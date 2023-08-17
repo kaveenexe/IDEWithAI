e0f98cf7dea02393ed1bbb90cc0ce80e8bec9a36
IT21108990 <IT21108990@my.sliit.lk>
2023-08-17 15:48:50 +0530

    login
    create user model
    frontend form (without styles)
    changes in server.js
    dbConnect file

--- frontend/src/pages/Register&Login/register.jsx
+++ frontend/src/pages/Register&Login/register.jsx
@@ -1,11 +1,81 @@
-import React from 'react'
+import { React, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 function Register() {
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function registerUser(event) {
+    event.preventDefault()
+    const response = await fetch("http://localhost:5000/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        fname: fname,
+        lname: lname,
+        email: email,
+        password: password,
+    })
+  })
+  const data = await response.json()
+  console.log(data)
+  }
+
   return (
     <div>
-        
+      <Form onSubmit={registerUser}>
+        <Row>
+          <Col>
+            <Form.Label>First Name</Form.Label>
+            <Form.Control  
+            value={fname}
+            onChange={(e) => setFname(e.target.value)}
+            placeholder="First name"
+            />
+            
+          </Col>
+          <Col>
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control placeholder="Last name" 
+            value={lname}
+            onChange={(e) => setLname(e.target.value)}
+            />
+            
+          </Col>
+        </Row>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" 
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          />
+          
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" 
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          />
+          
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
