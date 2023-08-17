4c118786a468426b33d1a63c30d159ae71078099
IT21108990 <IT21108990@my.sliit.lk>
2023-08-17 13:24:20 +0530

    bootstrap, packages, register page, routes in app.js
    added bootstrap
    backend packages(express, dotenv, mongoose, nodemon)
    forntend packages (react bootstrap, react drouter dom, )

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register&Login/register";
+import "./App.css";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <BrowserRouter>
+
+      {/* Navbar Hear */}
+
+        <Routes>
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
