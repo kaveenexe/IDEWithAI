07465590acdf1b36ee5d1e12f3a36f8a45e55d0b
IT21108990 <IT21108990@my.sliit.lk>
2023-08-18 16:57:26 +0530

    sign up & password hashing
    deleted old login and creating new one

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Register from "./pages/Register&Login/register";
+import Register from "./pages/Register&Login/Register";
+import Login from "./pages/Register&Login/login";
 import "./App.css";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
 
         <Routes>
           <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
