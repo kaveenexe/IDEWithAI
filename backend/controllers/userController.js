a6418c926b948d158f5d27a59269894693ebb4a5
IT21108990 <IT21108990@my.sliit.lk>
2023-08-22 14:42:58 +0530

    Making a useSignup Hook
    Error fixed
    Signup is working

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,40 +3,44 @@ const jwt = require("jsonwebtoken");
 
 // create token for 3 days
 const createToken = (_id) => {
-    return jwt.sign({_id: _id}, process.env.SECRET_KEY, {expiresIn: "3d"})
-}
+  return jwt.sign({ _id: _id }, process.env.SECRET_KEY, { expiresIn: "3d" });
+};
 
 //login user
 const loginUser = async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    try {
-        const user = await User.login(email, password);
+  try {
+    const user = await User.login(email, password);
 
-        //create a token
-        const token = createToken(user._id);
+    //create a token
+    const token = createToken(user._id);
 
-        res.status(200).json({ email, token });
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-}
-
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 // signup user
 const signupUser = async (req, res) => {
-    const { email, fname, lname, password } = req.body;
+  const { email } = req.body.email;
+  const { fname } = req.body.email;
+  const { lname } = req.body.email;
+  const { password } = req.body.email;
 
-    try {
-        const user = await User.signup(email, fname, lname, password);
+//   console.log("User Controller:", email);
 
-        //create a token
-        const token = createToken(user._id);
+  try {
+    const user = await User.signup(email, fname, lname, password);
 
-        res.status(200).json({ email, token }); 
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-}
+    //create a token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 module.exports = { loginUser, signupUser };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,32 +4,30 @@ const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema(
-  {
-    email: {
-      type: String,
-      required: true,
-      unique:true
-    },
-    fname: {
-      type: String,
-      required: true,
-    },
-    lname: {
-      type: String,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-  }
-);
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  fname: {
+    type: String,
+    required: true,
+  },
+  lname: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
 // ==== static signup method ====
-userSchema.statics.signup = async function(email, fname, lname, password) {
-
+userSchema.statics.signup = async function (email, fname, lname, password) {
   //validation
+  // console.log("User Model:", email);
   if (!email || !fname || !lname || !password) {
     throw Error("All fields are required");
   }
@@ -37,28 +35,29 @@ userSchema.statics.signup = async function(email, fname, lname, password) {
   if (!validator.isEmail(email)) {
     throw Error("Invalid email");
   }
+
   if (!validator.isStrongPassword(password)) {
-    throw Error("Password must be at least 8 characters long, contain at least 1 lowercase, 1 uppercase, 1 number and 1 symbol");
+    throw Error(
+      "Password must be at least 8 characters long, contain at least 1 lowercase, 1 uppercase, 1 number and 1 symbol"
+    );
   }
 
-
-  const exists = await this.findOne({ email})
+  const exists = await this.findOne({ email });
 
   if (exists) {
     throw Error("User already exists");
-  } 
+  }
 
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
 
   const user = await this.create({ email, fname, lname, password: hash });
 
-  return user
-}
+  return user;
+};
 
 //  ====  static login method ====
-userSchema.statics.login = async function(email, password) {
-
+userSchema.statics.login = async function (email, password) {
   //validation
   if (!email || !password) {
     throw Error("All fields are required");
@@ -77,7 +76,6 @@ userSchema.statics.login = async function(email, password) {
   }
 
   return user;
-}
-
+};
 
 module.exports = mongoose.model("User", userSchema);
