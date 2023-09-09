b044fa6020afbd10ae1eb68acc7e4517933e219a
IT21108990 <IT21108990@my.sliit.lk>
2023-09-09 20:42:14 +0530

    Home page

--- frontend/src/pages/Home/home.jsx
+++ frontend/src/pages/Home/home.jsx
@@ -1,13 +1,51 @@
-// src/components/Home.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './style.css'; // Import your CSS styles here
 
-const Home = () => {
+function Home() {
+  const [colorBoxes, setColorBoxes] = useState([]);
+
+  useEffect(() => {
+    const numberOfColorBoxes = 400;
+    const colorBoxElements = [];
+
+    for (let i = 0; i < numberOfColorBoxes; i++) {
+      colorBoxElements.push(
+        <div key={i} className="colorBox" />
+      );
+    }
+
+    setColorBoxes(colorBoxElements);
+  }, []);
+
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      {/* Add your Home page content here */}
+    <div className='main_homecontainer'>
+      <div className="bgAnimation" id="bgAnimation">
+        <div className="backgroundAmim"></div>
+      </div>
+      <div className="home_container">
+        <nav>
+          <h1><span>Dev</span>Mind</h1>
+          <ul>
+            <a href="/"><li>Home</li></a>
+            <a href="/code-editor"><li>Get Started</li></a>
+            <a href="/register"><li>Register</li></a>
+          </ul>
+        </nav>
+        <section>
+          <div className="textBox">
+            <h1><span>Make something great.</span></h1>
+            <br/>
+            <h4>Unlock Your Coding Potential with Our<br/>AI-Powered JavaScript IDE for Beginners!</h4>
+            <br />
+            <p>Are you eager to embark on your coding journey?<br/><br/>Discover the perfect companion in our online JavaScript IDE tailored for beginners.<br/>Whether you're taking your first coding steps or seeking guidance from seasoned coders, we've got you covered.</p>
+            <br />
+            <a href="/code-editor"><button className="homeBtn" style={{ '--i': '#fff' }}>Get Started</button></a>
+          </div>
+        </section>
+      </div>
+      {colorBoxes}
     </div>
   );
-};
+}
 
 export default Home;
