30f867cae715c1faa3cea6c9b759e188e7f368e6
IT21108990 <IT21108990@my.sliit.lk>
2023-09-16 23:39:22 +0530

    Update User DONE
    installed few packages
    npm install sweetalert2
    
    some navbar css
    dashboard css
    dashboard user update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 // create token for 3 days
 const createToken = (_id) => {
@@ -118,6 +119,43 @@ const getAllUsers = async (req, res) => {
   }
 };
 
+// =================== CRUD ===================
+
+// Update user
+const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { fname, lname, email, password, confirmPassword } = req.body;
+
+  try {
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    // Update user fields
+    if (fname) user.fname = fname;
+    if (lname) user.lname = lname;
+    if (email) user.email = email;
+
+    // Update password if provided and validate confirmPassword
+    if (password) {
+      if (password !== confirmPassword) {
+        return res.status(400).json({ error: 'Passwords do not match' });
+      }
+      const saltRounds = 10;
+      user.password = await bcrypt.hash(password, saltRounds);
+    }
+
+    // Save the updated user
+    await user.save();
+
+    res.status(200).json({ message: 'User updated successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
 module.exports = {
   loginUser,
   signupUser,
@@ -126,4 +164,5 @@ module.exports = {
   getUserByFname,
   // getUserFirstNameByEmail,
   getAllUsers,
+  updateUser
 };
