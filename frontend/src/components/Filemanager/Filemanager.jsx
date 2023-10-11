5f62da650d5807aba6b66817f36229402f1f8a78
Gehan Sathushka <it21102196@my.sliit.lk>
2023-10-12 02:27:47 +0530

    update file report

--- frontend/src/components/Filemanager/Filemanager.jsx
+++ frontend/src/components/Filemanager/Filemanager.jsx
@@ -6,6 +6,7 @@ import { Modal, Button } from 'react-bootstrap';
 import Swal from 'sweetalert2';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { PDFDocument, rgb,StandardFonts } from 'pdf-lib';
 
 
 const Filemanager = ({ onFileSelect }) => {
@@ -100,29 +101,83 @@ const Filemanager = ({ onFileSelect }) => {
   };
 
 
-  const handleGenerateReport = (content) => {
-    if (content) {
+  const handleGenerateReport = () => {
+    if (selectedFileDetails && content) {
       const codeDetails = extractCodeDetails(content);
-      const report = generateReport(content, codeDetails);
+      const report = generateReport(selectedFileDetails, codeDetails);
       setGeneratedReport(report);
-      console.log(report);
-    } else {
-      // Handle the case when content is undefined
-      // For example, you can show an error message or handle it as needed.
-      console.error('Content is undefined or null.');
-    }
-  };  
 
-  const generateReport = (content, codeDetails) => {
+      // Generate and download the PDF report
+      generateAndDownloadPDF(report);
+    } else {
+      console.error('Content or file details are undefined.');
+    }
+  };
+  
+
+  const generateReport = (fileDetails, codeDetails) => {
     return {
-      fileName: selectedFileDetails.name,
-      fileContent: content,
-      createdDate: new Date(selectedFileDetails.createdDate).toLocaleDateString(),
-      lastModifiedDate: new Date(selectedFileDetails.lastModifiedDate).toLocaleDateString(),
+      fileName: fileDetails.name,
+      createdDate: new Date(fileDetails.createdDate).toLocaleDateString(),
+      lastModifiedDate: new Date(fileDetails.lastModifiedDate).toLocaleDateString(),
       fileType: 'JavaScript',
       keywords: codeDetails.keywords,
       variables: codeDetails.variables,
     };
+  };
+
+  const generateAndDownloadPDF = async (report,fileDetails) => {
+    const pdfDoc = await PDFDocument.create();
+    const page = pdfDoc.addPage([600, 400]);
+    const content = generateReportContent(report);
+    const headerContent = "DEVMIND";
+    page.drawText(headerContent, { x: 200, y: 350, size: 50, color: rgb(0, 0, 1), alignment: 'center', lineHeight: 16, font: await pdfDoc.embedFont(StandardFonts.Helvetica), });
+    
+    const headerText2 = fileDetails.name + "'s Report";
+    page.drawText(headerText2, {
+      x: 210,
+      y: 310, // Adjust the y position as needed
+      size: 30, // Adjust the font size as needed
+      color: rgb(0, 0, 0), // Black color
+      font: await pdfDoc.embedFont(StandardFonts.Helvetica),
+      lineHeight: 12,
+      maxWidth: 500,
+      alignment: 'center',
+    });
+
+
+
+    page.drawText(content, {
+      x: 50,
+      y: page.getHeight() - 100,
+      size: 14,
+      color: rgb(0, 0, 0),
+    });
+
+    // Save the PDF bytes and download the file
+    const pdfBytes = await pdfDoc.save();
+    downloadPDF(pdfBytes, `${selectedFileDetails.name}_report.pdf`);
+  };
+
+  const downloadPDF = (pdfBytes, filename) => {
+    const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+    const link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.download = filename;
+    link.click();
+  };
+
+
+  const generateReportContent = (report) => {
+    // Customize this as needed to format your report content
+    return `
+      File Name: ${report.fileName}
+      Created Date: ${report.createdDate}
+      Last Modified Date: ${report.lastModifiedDate}
+      File Type: ${report.fileType}
+      Keywords: ${report.keywords.join(', ')}
+      Variables: ${report.variables.join(', ')}
+    `;
   };
 
   return (
